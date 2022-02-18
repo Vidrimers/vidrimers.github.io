b86944f0a97a2a57ac7ebe440c0df6d65a709e74
Vidrimers <vidrimers2@gmail.com>
2022-02-19 01:32:33 +0300

    new photo hero

---
+++ dist/assets/js/app.js
@@ -0,0 +1,12 @@
+console.log("created by Shiryakov Yaroslav");
+
+
+// //= components/tabs.js
+// //= components/modal.js
+
+
+// //= ../../../node_modules/slick-carousel/slick/slick.js
+
+// $(function () {
+// //= components/slick.js
+// })
