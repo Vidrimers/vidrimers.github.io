5f5a2bc12855c91566e8ac57192fbb58237e98f9
Vidrimers <vidrimers2@gmail.com>
2025-09-17 18:33:12 +0300

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -10735,4 +10735,19 @@ return jQuery;
       $('.hero__menu-list').removeClass('hero__menu-list--active');
       $('body').removeClass('lock');
   });
+console.log("portfolio-tabs.js is loaded");
+
+$(document).ready(function () {
+  $(".portfolio__tabs-item").click(function (e) {
+    e.preventDefault();
+
+    $(".portfolio__tabs-item").removeClass("portfolio__tabs-item--active");
+    $(".portfolio__items").removeClass("portfolio__items--active");
+
+    $(this).addClass("portfolio__tabs-item--active");
+    $($(this).attr("href")).addClass("portfolio__items--active");
+  });
+
+  $(".portfolio__tabs-item:first").click();
+});
 })
